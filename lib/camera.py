7a7643f1f39be5b57f5cdd08c4d1179ea8e54ff7
super-cow-powers <dm1306@york.ac.uk>
2020-04-16 17:21:23 +0100

    launcher good enough

--- lib/camera.py
+++ lib/camera.py
@@ -1,2 +1,6 @@
+def required_conf(): #Returns a list of the required conf files
+    list = ["serial_dev.conf", "camera.conf"]
+    return list
+
 def camera():
     print("Camera Module\n")
--- lib/graph.py
+++ lib/graph.py
@@ -1,2 +1,7 @@
+def required_conf(): #Returns a list of the required conf files
+    list = ["serial_dev.conf"]
+    return list
+
+
 def graph():
     print("graph Module\n")
